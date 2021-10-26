434a299a4ae7934d15532bdd7efb7fb34c795efa
Akis <b_kalogeropoulos@yahoo.gr>
2021-10-26 13:26:28 +0300

    add react-native-elements and set up Sign up screen

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,18 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, Button } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { Text, Input, Button } from 'react-native-elements';
+import Spacer from '../components/Spacer';
 
 const SignUpScreen = ({ navigation }) => {
     return (
         <>
-            <Text>SignUp Screen</Text>
+            <Spacer>
+            <Text h3>Sign Up for Tracker</Text>
+            </Spacer>
+            <Input label="Email" />
+            <Input label="Password" />
+            <Spacer>
             <Button
-                title= "Sign in"
+                title= "Sign up"
                 onPress={() => navigation.navigate('Signin')}
             />
+            </Spacer>
+            <Spacer>
             <Button
                 title= "Main flow"
                 onPress={() => navigation.navigate('mainFlow')}
             />
+            </Spacer>
         </>
     );
 };
