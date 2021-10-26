6fb2cb0407513663a4b9aa7b506707b0c8285829
Akis <b_kalogeropoulos@yahoo.gr>
2021-10-26 14:22:37 +0300

    Set up Context

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import react from "react";
+import React from "react";
 import { createAppContainer, createSwitchNavigator } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import { createBottomTabNavigator } from "react-navigation-tabs";
@@ -8,7 +8,7 @@ import SignUpScreen from "./src/screens/SignUpScreen";
 import TrackCreateScreen from "./src/screens/TrackCreateScreen";
 import TrackDetailScreen from "./src/screens/TrackDetailScreen";
 import TrackListScreen from "./src/screens/TrackListScreen";
-
+import { Provider as AuthProvider } from "./src/context/AuthContext";
 
 const switchNavigator = createSwitchNavigator({
   loginFlow: createStackNavigator({
@@ -25,4 +25,12 @@ const switchNavigator = createSwitchNavigator({
   })
 });
 
-export default createAppContainer(switchNavigator);
+const App = createAppContainer(switchNavigator);
+
+export default () => {
+  return(
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  );
+}
