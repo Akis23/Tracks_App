acd5f42710d84ea50060f6f9157d9d4627734e0d
Akis <b_kalogeropoulos@yahoo.gr>
2021-11-01 13:08:30 +0200

    Set up sign out in AccountScreen

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -13,6 +13,8 @@ const authReducer = (state, action) => {
             return { errorMessage: '', token: action.payload };
         case 'clear_error_message':
             return { ...state, errorMessage: '' };
+        case 'signout':
+            return { token: null, errorMessage: '' };
         default:
             return state;
     }
@@ -67,8 +69,10 @@ const signin = (dispatch) => {
 };
 
 const signout = (dispatch) => {
-    return ({ email, password }) => {
-
+    return async ({ email, password }) => {
+        await AsyncStorage.removeItem('token');
+        dispatch({ type: 'signout'});
+        navigate('loginFlow');
     };
 };
 
--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useContext } from 'react';
+import { StyleSheet } from 'react-native';
+import { Button, Text } from 'react-native-elements';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Spacer from '../components/Spacer';
+import { Context as AuthContext } from '../context/AuthContext';
 
 const AccountScreen = () => {
+    const { signout } = useContext(AuthContext);
+
     return (
-        <View>
-            <Text>Account Screen</Text>
-        </View>
+        <SafeAreaView forceInset={{ top: 'always' }}>
+            <Text h1>Account</Text>
+            <Spacer>
+                <Button 
+                    title="Sign Out"
+                    onPress={signout}
+                />
+            </Spacer>
+        </SafeAreaView>
     );
 };
 
 const styles = StyleSheet.create({});
 
-export default AccountScreen
+export default AccountScreen;
