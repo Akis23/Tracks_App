f3255b498dd67a6d4a29d1b9bb37e6ac9eab080f
Akis <b_kalogeropoulos@yahoo.gr>
2021-11-01 10:10:24 +0200

    reusable authform component for signIn and signUp

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://e078-2a02-587-b480-2100-7197-b5a3-e606-6a4.ngrok.io'
+    baseURL: 'http://be90-2a02-587-b480-2100-fd94-2e01-5087-f5e5.ngrok.io'
 });
--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,46 +1,25 @@
-import React, { useState, useContext } from 'react';
+import React, { useContext } from 'react';
 import { StyleSheet, View, TouchableOpacity, Touchable } from 'react-native';
-import { Text, Input, Button } from 'react-native-elements';
+import { Text } from 'react-native-elements';
 import Spacer from '../components/Spacer';
 import { Context as AuthContext } from '../context/AuthContext';
+import AuthForm from '../components/AuthForm';
 
 const SignUpScreen = ({ navigation }) => {
     const { state, signup } = useContext(AuthContext);
-    const [ email, setEmail ] = useState('');
-    const [ password, setPassword ] = useState('');
+    
 
 
     return (
         <View style={styles.container}>
-            <Spacer>
-            <Text h3>Sign Up for Tracker</Text>
-            </Spacer>
-            <Spacer />
-            <Input
-                label="Email"
-                value={email}
-                onChangeText={setEmail}
-                autoCapitalize="none"
-                autoCorrect={false}
+            <AuthForm 
+                headerText="Sign Up for Tracker"
+                errorMessage={state.errorMessage}
+                submitButtonText="Sign Up"
+                onSubmit={({ email, password }) => signup({ email, password })}
             />
-            <Input
-                // for password
-                secureTextEntry
-                label="Password"
-                value={password}
-                onChangeText={setPassword}
-                autoCapitalize="none"
-                autoCorrect={false}
-            />
-            { state.errorMessage ? (<Text style={styles.errorMessage}>{state.errorMessage}</Text>) : null }
-            <Spacer>
-            <Button
-                title= "Sign up"
-                onPress={() => signup({ email, password })}
-            />
-            </Spacer>
             <TouchableOpacity onPress={() => navigation.navigate('Signin')}>
-                <Text style={styles.link}>Already have an account? Sign in instead!</Text>
+                <Text style={styles.link} >Already have an account? Sign in instead!</Text>
             </TouchableOpacity>
         </View>
     );
